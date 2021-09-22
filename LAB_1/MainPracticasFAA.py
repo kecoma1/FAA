e402601eaf8027e118d203c2619a0d4b2fa1a560
kecoma1 <47478959+kecoma1@users.noreply.github.com>
2021-09-22 12:10:51 +0200

    main entrega

--- LAB_1/MainPracticasFAA.py
+++ LAB_1/MainPracticasFAA.py
@@ -3,26 +3,33 @@ import pandas as pd
 import EstrategiaParticionado
 
 # Cargamos los datasets
+print("Cargando dataset tic-tac-toe ...")
 dataset1=Datos('ConjuntosDatos/tic-tac-toe.data')
+
+print("Cargando dataset german ...")
 dataset2=Datos('ConjuntosDatos/german.data')
 
 # Cargamos los datos en un dataframe
+print("Cargando dataframe tic-tac-toe ...")
 df1 = pd.read_csv("ConjuntosDatos/tic-tac-toe.data")
+
+print("Cargando dataframe german ...")
 df2 = pd.read_csv("ConjuntosDatos/german.data")
 
 # TICTACTOE
 # Comparamos los datos con el dataset construido
-print("DATOS:\n", dataset1.datos[:3])
+print("[TIC-TAC-TOE] DATOS:\n", dataset1.datos[:3])
 print(df1.head(3))
-print("NOMINAL ATRIBUTOS:\n", dataset1.nominalAtributos, "\n")
+print("[TIC-TAC-TOE] NOMINAL ATRIBUTOS:\n", dataset1.nominalAtributos, "\n")
 
-print("DICCIONARIO ITEMS:")
+print("[TIC-TAC-TOE] DICCIONARIO ITEMS:")
 for dicc in dataset1.diccionario.items():
     print(dicc)
 
-print("\nDICCIONARIO ENTERO:\n"+str(dataset1.diccionario))
+print("\n[TIC-TAC-TOE] DICCIONARIO ENTERO:\n"+str(dataset1.diccionario))
 
 # Probamos las estrategias de particionado
+print("[TIC-TAC-TOE] VALIDACIÓN SIMPLE")
 vc = EstrategiaParticionado.ValidacionSimple(10, 5) 
 vc.creaParticiones(dataset1.datos)
 
@@ -31,6 +38,7 @@ for i in range(5):
     print("***Test***:", vc.particiones[i].indicesTest)
     print("***Train***:", vc.particiones[i].indicesTrain)
 
+print("[TIC-TAC-TOE] VALIDACIÓN CRUZADA")
 vc = EstrategiaParticionado.ValidacionCruzada(10) 
 vc.creaParticiones(dataset1.datos)
 
@@ -43,18 +51,18 @@ print("\n-----------------------------------------------------------------------
 print("-----------------------------------------------------------------------------------\n")
 # GERMAN
 # Comparamos los datos con el dataset construido
-print("DATOS:\n", dataset2.datos[:3])
+print("[GERMAN] DATOS:\n", dataset2.datos[:3])
 print(df2.head(3))
-print("NOMINAL ATRIBUTOS:\n", dataset2.nominalAtributos, "\n")
+print("[GERMAN] NOMINAL ATRIBUTOS:\n", dataset2.nominalAtributos, "\n")
 
-print("DICCIONARIO ITEMS:")
+print("[GERMAN] DICCIONARIO ITEMS:")
 for dicc in dataset2.diccionario.items():
     print(dicc)
 
-print("\nDICCIONARIO ENTERO:\n"+str(dataset2.diccionario))
+print("\n[GERMAN] DICCIONARIO ENTERO:\n"+str(dataset2.diccionario))
 
 # Probamos las estrategias de particionado
-print("VALIDACIÓN SIMPLE")
+print("[GERMAN] VALIDACIÓN SIMPLE - GERMAN")
 vc = EstrategiaParticionado.ValidacionSimple(10, 5) 
 vc.creaParticiones(dataset1.datos)
 for i in range(5):
@@ -62,7 +70,7 @@ for i in range(5):
     print("***Test***:", vc.particiones[i].indicesTest)
     print("***Train***:", vc.particiones[i].indicesTrain)
 
-print("VALIDACIÓN CRUZADA")
+print("[GERMAN] VALIDACIÓN CRUZADA - GERMAN")
 vc = EstrategiaParticionado.ValidacionCruzada(10) 
 vc.creaParticiones(dataset1.datos)
 for i in range(10):
