95fb37b4969c05a128addfb2c490d97489e16835
kecoma1 <kecoma2@gmail.com>
2021-10-02 20:23:23 +0200

    Media y varianza incluidas

--- LAB_2/Clasificador.py
+++ LAB_2/Clasificador.py
@@ -86,6 +86,10 @@ class ClasificadorNaiveBayes(Clasificador):
 		...		...									...
 
 		Estos mismos atributos también se crean pero aplicando la corrección de LaPlace (CLP).
+
+		En el caso de los atributos continuos se guarda una lista con 2 elementos, la media y la varianza:
+		...		Atr1
+		CX		[media, varianza] # En este mismo orden
 	"""
 
 	def __init__(self):
@@ -113,7 +117,7 @@ class ClasificadorNaiveBayes(Clasificador):
 				diccionario: 
 		"""
 		# Calculamos la probabilidad de las clases
-		self.calculaPClases(datostrain, diccionario)
+		self.calculaPClases(datostrain)
 
 		# Calculamos las probabilidades condicionadas
 		self.calculaPCondicionales(datostrain, atributosDiscretos, diccionario)
@@ -123,12 +127,12 @@ class ClasificadorNaiveBayes(Clasificador):
 	def clasifica(self, datostest, atributosDiscretos, diccionario):
 		pass
 
-	def calculaPClases(self, datostrain, diccionario):
+
+	def calculaPClases(self, datostrain):
 		"""Método para calcular la probabilidad de las clases (frecuencia).
 
 		Args:
 			datostrain (Matriz Numpy): Datos a comprobar.
-			diccionario (dict): Diccionario con los atributos y las clases.
 		"""
 		# Obtenemos la frecuencia de los valores en las columnas
 		_, self.freq = list(np.unique(datostrain[:,-1], return_counts=True))
@@ -137,6 +141,13 @@ class ClasificadorNaiveBayes(Clasificador):
 
 
 	def calculaPCondicionales(self, datostrain, atributosDiscretos, diccionario):
+		"""Método para calcular las probabilidades condicionales.
+
+		Args:
+			datostrain (numpy.array): Matriz con los datos.
+			atributosDiscretos (list): Columnas de la matriz de datos
+			diccionario (dict): Diccionario con información sobre los atributos y la clase.
+		"""
 		# Calculamos el número de filas y columnas de la matriz 
 		# con las probabilidades condicionadas
 		nCols = datostrain.shape[1]-1
@@ -155,7 +166,7 @@ class ClasificadorNaiveBayes(Clasificador):
 
 		# Calculamos las probabilidades condicionales
 		for classIndex in range(nRows): # Por cada valor de la clase
-			# Obtenemos el valor específico de la clase
+			# Obtenemos el valor específico de la clase a comprobar
 			if atributosDiscretos[-1]:
 				classValue =list(list(diccionario.items())[-1][1].values())[classIndex]
 			else:
@@ -164,28 +175,35 @@ class ClasificadorNaiveBayes(Clasificador):
 				# Query para obtener unicamente las filas con la clase deseada
 				rowsToCheck = np.where(datostrain[:,-1]==classValue)
 				if not atributosDiscretos[atrIndex]: # En caso de que no sea discreto calculamos la probabilidad continua
-					continue # calcula probabilidad_continua
+					self.calculaPCondicionalContinua(atrIndex, classIndex, datostrain)
 				else:
-					if atrIndex == len(diccionario)-1: # En caso de que lleguemos a la clase
-						break
-					else:
-						self.calculaPCondicionalDiscreta(atrIndex, classIndex, classValue, rowsToCheck, datostrain, diccionario)
+					self.calculaPCondicionalDiscreta(atrIndex, classIndex, rowsToCheck, datostrain, diccionario)
 
 
-	def calculaPCondicionalDiscreta(self, atrIndex, classIndex, classValue, rowsToCheck, datostrain, diccionario):
+	def calculaPCondicionalDiscreta(self, atrIndex, classIndex, rowsToCheck, datostrain, diccionario):
+		"""Método para calcular la probabilidad condicional de un atributo discreto.
+
+		Args:
+			atrIndex (int): Índice del atributo.
+			classIndex (int): Índice de la clase.
+			rowsToCheck (numpy.array): Array con el índice de las filas que contienen el valor de la clase correcto.
+			datostrain (numpy.array): Matriz con los datos.
+			diccionario (dict): Diccionario con información sobre los atributos y la clase.
+		"""
 		# Declaramos el array interno de cada atributo_i|clase
 		uniqueInCol = len(list(diccionario.values())[atrIndex].values())
 		self.pCondicionales[classIndex][atrIndex] = np.zeros(uniqueInCol)
-		self.CLPpCondicionales[classIndex][atrIndex] = np.zeros(uniqueInCol)
+		self.CLPpCondicionales[classIndex][atrIndex] = np.zeros(uniqueInCol) # Array laplace
 		freqs = np.zeros(uniqueInCol)
 
 		for atrValue in range(uniqueInCol): # Por cada valor en el atributo
 			for row in datostrain[rowsToCheck]: # Comprobamos cada la fila
-				if row[-1] == classValue and row[atrIndex] == atrValue:
+				if row[atrIndex] == atrValue:
 					self.pCondicionales[classIndex][atrIndex][atrValue] += 1
 			freqs[atrValue] = self.pCondicionales[classIndex][atrIndex][atrValue]
 			self.pCondicionales[classIndex][atrIndex][atrValue] /= self.freq[classIndex]
 
+		# En caso de que haya un valor a 0 en las frecuencias, aplicamos la corrección de laplace
 		if 0 in self.pCondicionales[classIndex][atrIndex]:
 			for i in range(uniqueInCol):
 				# Para calcular la correción de laplace añadimos 1
@@ -193,3 +211,18 @@ class ClasificadorNaiveBayes(Clasificador):
 				self.CLPpCondicionales[classIndex][atrIndex][i] /= (self.freq[classIndex] + len(self.CLPpCondicionales[classIndex][atrIndex]))
 		else:
 			self.CLPpCondicionales[classIndex][atrIndex] = np.copy(self.pCondicionales[classIndex][atrIndex])
+
+
+	def calculaPCondicionalContinua(self, atrIndex, classIndex, datostrain):
+		"""Método para calcular la probabilidad continua.
+
+		Args:
+			atrIndex (int): Índice del atributo.
+			classIndex (int): Índice de la clase.
+			datostrain (numpy.array): Matriz con los datos.
+		"""
+		self.pCondicionales[classIndex][atrIndex] = np.zeros(2)
+
+		# Guardamos la media y la varianza
+		self.pCondicionales[classIndex][atrIndex][0] = np.mean(datostrain[:,atrIndex])
+		self.pCondicionales[classIndex][atrIndex][1] = np.var(datostrain[:,atrIndex])
