dc85dbb4f8a403ea95700ba27413966017822388
kecoma1 <kecoma2@gmail.com>
2021-10-18 19:11:07 +0200

    Primera aproximación K-NN

--- LAB_3/ClasificadorKNN.py
+++ LAB_3/ClasificadorKNN.py
@@ -12,13 +12,17 @@ class ClasificadorKNN(Clasificador):
 
 	def entrenamiento(self, datostrain, atributosDiscretos, diccionario):
 		# Normalizamos los datos
-		datosNormalizados = self.normalizarDatos(datostrain, atributosDiscretos)
+		self.datosNormalizados = self.normalizarDatos(datostrain, atributosDiscretos)
 
 		# Calculamos la probabilidad de las clases
-		self.calculaPClases(datostrain)
+		self.calculaPClases(self.datosNormalizados)
 
 	def clasifica(self, datostest, atributosDiscretos, diccionario):
-		pass
+		prediccionesClases = []
+		testNormalizados = self.normalizarDatos(datostest, atributosDiscretos)
+		for rowTest in testNormalizados: # Por cada fila en el test
+			prediccionesClases.append(self.clasificaFila(rowTest))
+		return prediccionesClases
 
 	def calculaPClases(self, datostrain):
 		"""Método para calcular la probabilidad de las clases (frecuencia).
@@ -27,10 +31,9 @@ class ClasificadorKNN(Clasificador):
 			datostrain (Matriz Numpy): Datos a comprobar.
 		"""
 		# Obtenemos la frecuencia de los valores en las columnas
-		_, self.freqs = np.unique(datostrain[:,-1], return_counts=True)
+		_, self.freq = np.unique(datostrain[:,-1], return_counts=True)
 		self.N = sum(self.freq)
-		self.pClases = np.array([i/self.N for i in self.freqs])
-
+		self.pClases = np.array([i/self.N for i in self.freq])
 
 	def calcularMediasDesv(self, datos, nominalAtributos):
 		"""Función para calcular las medias y las desviaciones típicas.
@@ -62,14 +65,52 @@ class ClasificadorKNN(Clasificador):
 		"""
 		mediasDevs = self.calcularMediasDesv(datos, nominalAtributos)
 		datosNormalizados = np.zeros(shape=datos.shape)
-		for i, row in enumerate(datos):
-			for j in range(len(row)):
-				if nominalAtributos[j]:
-					continue
-				datosNormalizados[i][j] = norm(mediasDevs[j][0], mediasDevs[j][1]).pdf(datos[i][j])
+		for numCol, col in enumerate(datos.transpose()):
+			if nominalAtributos[numCol] or numCol == datos.shape[1]-1:
+				datosNormalizados[:,numCol] = col
+			else:
+				for numRow in range(len(col)):
+					datosNormalizados[numRow][numCol] = norm(mediasDevs[numCol][0], mediasDevs[numCol][1]).pdf(datos[numRow][numCol])
 		return datosNormalizados
 
-	def __distanciaEuclidea(self, x, y, w=None):
+	def clasificaFila(self, fila):
+		"""Dada una fila del test, buscamos las K clases más cercanas a la fila.
+
+		Args:
+			fila: Fila del test a comprobar
+
+		Returns:
+			Devuelve la clase adecuada dada la fila.
+		"""
+		distancias = [] # Lista con tuplas. [(distancia, clase), ...]
+		# Calculamos las distancias
+		for rowData in self.datosNormalizados:
+			distancias.append((self.distanciaEuclidea(fila, rowData), rowData[-1]))
+
+		return self.getClaseKVecinos(distancias)
+
+	def getClaseKVecinos(self, distancias):
+		"""Dadas unas distancias se devuelve la clase más frecuente.
+
+		Args:
+			distancias (list): Lista de tuplas que contiene (distancia, clase)
+
+		Returns:
+			float: Clase
+		"""
+		distanciasOrdenadas = sorted(distancias, key=lambda x: x[0])
+		return  self.getClaseMasFrequente(distanciasOrdenadas[:self.K])
+
+	def getClaseMasFrequente(self, distancias):
+		clases = {}
+		for _, c in distancias:
+			if c not in clases:
+				clases[c] = 1
+			else:
+				clases[c] += 1
+		return sorted(clases.items(), key=lambda x: x[1])[0][0]
+
+	def distanciaEuclidea(self, x, y, w=None):
 		"""Función privada para calcular la distancia euclidea
 		entre 2 vectores. También se calcula la distancia euclidea
 		ponderada en caso de que se pasen las ponderaciones.
@@ -87,7 +128,7 @@ class ClasificadorKNN(Clasificador):
 		else:
 			return sum([((xi-yi)*wi)**2 for xi, yi, wi in zip(x, y, w)])**(1/2)
 
-	def __distanciaManhattan(self, x, y):
+	def distanciaManhattan(self, x, y):
 		"""Función privada para calcular la distancia manhattan
 		entre 2 vectores.
 
@@ -100,7 +141,7 @@ class ClasificadorKNN(Clasificador):
 		"""
 		return [math.abs(xi-yi) for xi, yi in zip(x, y)]
 
-	def __distanciaChevychev(self, x, y):
+	def distanciaChevychev(self, x, y):
 		"""Función privada para calcular la distancia manhattan
 		entre 2 vectores.
 
