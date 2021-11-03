ca1b906082b4969105c77a1fcb297400694e4247
kecoma1 <kecoma2@gmail.com>
2021-11-03 12:05:46 +0100

    Funciones para calcular la matriz de confusión hechas. NEED TO TEST

--- LAB_3/KMeans.py
+++ LAB_3/KMeans.py
@@ -157,9 +157,172 @@ def kMeans(k, datos):
 	return clusters
 
 
-def error(k, clusters, datos):
-	clases = np.unique(datos[:,-1])
+def confianzas(clusters, datos):
+	"""Función para obtener la "confianza" de todos los clusters.
+
+	Args:
+		clusters (list): Lista con los clusters.
+		datos (numpy.array): Dataset.
+
+	Returns:
+		list: Lista con todas las confianzas
+	"""
+	confianzas = []
+	numClases = len(np.unique(datos[:,-1]))
+
+	# Analizamos la confianza de cada cluster
 	for cluster in clusters.values():
-		frecuenciaClases = np.zeros(len(clases))
-		for value in cluster:
-			frecuenciaClases[datos[value][-1]] += 1
+		confianzas.append(confianzaCluster(cluster, datos, numClases))
+
+	return confianzas
+
+
+def confianzaCluster(cluster, datos, numClases):
+	"""Función para calcular la confianza en específico de un cluster.
+	Para calcular esta confianza se obtiene la clase mayoritaria en
+	el cluster para posteriormente calcular el porcentaje de está clase
+	dentro del cluster.
+
+	Args:
+		cluster (list): Lista con las filas de los datos agrupadas en el cluster.
+		datos (numpy.array): Dataset.
+		numClases (int): Cantidad de clases en el dataset.
+
+	Returns:
+		(float, int): Porcentaje de la clase mayoritaria en el cluster y la clase mayoritaria.
+	"""
+	clasesFreq = {i: 0 for i in range(numClases)}
+
+	# Analizamos la frecuencia de las clases en el cluster
+	for dato in cluster:
+		claseDato = datos[dato][-1]
+		clasesFreq[int(claseDato)] += 1
+
+	claseMayoritaria, freqMayoritaria = max(clasesFreq.items(), key=lambda x: x[1])
+	return (freqMayoritaria/sum(clasesFreq.values()), claseMayoritaria)
+
+
+def confianzaMedia(confianzas):
+	"""Función para obtener la media de todas las confianzas.
+
+	Args:
+		confianzas (list): Lista con las confianzas de varios clusters.
+
+	Returns:
+		Float: Media de las confianzas.
+	"""
+	avg = 0
+	for confianza in confianzas:
+		avg += confianza[0]
+
+	return avg/len(confianzas)
+
+
+def matrizConfusionCluster(cluster, datos, clase, resInProb=False):
+	"""Función que dado un cluster, devuelve los datos de una matríz de confusión.
+
+	Args:
+		cluster (tuple): Cluster a analizar.
+		datos (numpy.array): Dataset
+		clase (int): Clase mayoritaria en el cluster
+		resInProb (bool): Variable opcional para obtener los valores 
+		con valores entre 0-1
+
+	Returns:
+		tuple: Verdaderos positivos, verdaderos negativos, falsos positivos, falsos negativos.
+	"""
+	lenCluster = 1
+	lenDatos = 1
+	vp = verdaderosPositivos(cluster, datos, clase)
+	vn = verdaderosNegativos(cluster, datos, clase)
+	fp = falsosPositivos(cluster, datos, clase)
+	fn = falsosNegativos(cluster, datos, clase)
+
+	if resInProb:
+		lenCluster = len(cluster)
+		lenDatos = len(datos)
+
+	# Si resInProb es False, lenDatos=1 y lenCluster=1.
+	return vp/lenCluster, vn/lenDatos, fp/lenCluster, fn/lenDatos
+
+
+def verdaderosPositivos(cluster, datos, clase):
+	"""Función para calcular los verdaderos positivos.
+	Obtenemos el número de datos dentro del cluster
+	que pertenece a la clase mayoritaria.
+
+	Args:
+		cluster (list): Lista con los indices de los datos en el cluster.
+		datos (numpy.array): Dataset.
+		clase (int): Clase mayoritaria del cluster.
+
+	Returns:
+		int: Número de verdaderos positivos en el cluster.
+	"""
+	vp = 0
+	for dato in cluster:
+		if int(datos[dato][-1]) == clase:
+			vp += 1
+	return vp 
+
+
+def verdaderosNegativos(cluster, datos, clase):
+	"""Función para calcular los verdaderos negativos.
+	Obtenemos el número de datos en el dataset que:
+		1. No están en el cluster
+		2. La clase es distinta
+
+	Args:
+		cluster (list): Lista con los indices de los datos en el cluster.
+		datos (numpy.array): Dataset.
+		clase (int): Clase mayoritaria del cluster.
+
+	Returns:
+		int: Número de verdaderos negativos en el cluster.
+	"""
+	vn = 0
+	for i, fila in enumerate(datos):
+		if fila[-1] != clase and i not in cluster:
+			vn += 1
+	return vn
+
+
+def falsosPositivos(cluster, datos, clase):
+	"""Función para calcular los falsos positivos.
+	Obtenemos el número de datos en el cluster que
+	no son de la clase mayoritaria.
+
+	Args:
+		cluster (list): Lista con los indices de los datos en el cluster.
+		datos (numpy.array): Dataset.
+		clase (int): Clase mayoritaria del cluster.
+
+	Returns:
+		int: Número de falsos positivos en el cluster.
+	"""
+	fp = 0
+	for dato in cluster:
+		if int(datos[dato][-1]) != clase:
+			fp += 1
+	return fp
+
+
+def falsosNegativos(cluster, datos, clase):
+	"""Función para calcular los falsos negativos.
+	Obtenemos el número de datos en el dataset que:
+		1. No están en el cluster.
+		2. Son de la clase mayoritaria del cluster.
+
+	Args:
+		cluster (list): Lista con los indices de los datos en el cluster.
+		datos (numpy.array): Dataset.
+		clase (int): Clase mayoritaria del cluster.
+
+	Returns:
+		int: Número de falsos negativos en el cluster.
+	"""
+	fn = 0
+	for i, fila in enumerate(datos):
+		if fila[-1] == clase and i not in cluster:
+			fn += 1
+	return fn
